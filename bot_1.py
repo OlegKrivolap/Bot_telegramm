687db0a672dbc8b13b1413634b9fa556bb7627f9
forsterit <olegkrivolap57@gmail.com>
2023-10-19 22:55:17 +0300

    Found

--- bot_1.py
+++ bot_1.py
@@ -1,3 +1,4 @@
+from aiogram.methods.answer_callback_query import AnswerCallbackQuery
 from aiogram import Bot, Dispatcher, Router, types
 from token_1 import TOKEN_my
 import asyncio
@@ -6,12 +7,21 @@ from aiogram import F
 
 bot = Bot(token=TOKEN_my)
 dp = Dispatcher()
-router = Router(name=__name__)
+router = Router()
 
-b1 = types.KeyboardButton(text='Money')
-b2 = types.KeyboardButton(text='Honey')
-kb = types.ReplyKeyboardMarkup(keyboard=[[b1, b2]], resize_keyboard=True)
+buttons = [types.KeyboardButton(text='Money'),
+           types.KeyboardButton(text='Honey')
+           ]
+kb = types.ReplyKeyboardMarkup(keyboard=[[*buttons]], resize_keyboard=True)
 
+buttons_1 = [
+    types.InlineKeyboardButton(text='Super', callback_data='send'),
+    types.InlineKeyboardButton(text='Puper', url='https://www.youtube.com/watch?v=ce1idtN6-wQ&t=3493s')
+]
+
+inkb = types.InlineKeyboardMarkup(inline_keyboard=[[*buttons_1]])
+
+rmkb = types.ReplyKeyboardRemove()
 
 
 
@@ -21,18 +31,27 @@ async def echo(message: types.Message):
 
 @dp.message(Command(commands='stop'))
 async def echo(message: types.Message):
-    await message.answer(text='Salamchick')
+    await message.answer(text='До свидания', reply_markup=rmkb)
 
+
+@dp.message(F.text == 'button')
+async def inly(message: types.Message):
+    await message.answer(text='Клава супер',reply_markup=inkb )
 
 @dp.message(F.photo)
 async def echo(message: types.Message):
     await message.answer('Крутая фотка')
 
 
-@router.message(F.text(equals='Money'))
+@dp.message(F.text == 'Money')
 async def but_money(message: types.Message):
-    await message.answer(text=f'Была нажата кнопка {message}')
+    await message.answer(text=f'Была нажата кнопка Money')
 
+
+@dp.callback_query(F.data == 'send')
+async def inkey(callback: types.CallbackQuery):
+    await callback.message.answer('Наконец-то')
+    await callback.answer('Запрос обработан')
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
